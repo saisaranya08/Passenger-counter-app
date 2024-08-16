7ec14abe71a3dfdd9621dd3fbeaaad1aa8c19f02
saisaranya08 <saisaranyac123@gmail.com>
2024-08-16 22:30:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+console.log(countEl)
+function increment(){
+    count +=1
+    countEl.textContent = count
+
+}
+function save(){
+    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = count
+
+}
+
+
+
+
